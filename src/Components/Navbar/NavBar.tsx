0be40ed7588fc073adf65dc06fab0588168f69f0
Jofre Tomas <jofretomas9@gmail.com>
2024-09-16 20:30:29 -0300

    Responsividade incluidas para diversas telas

--- src/Components/Navbar/NavBar.tsx
+++ src/Components/Navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BiSearch } from "react-icons/bi";
 import { PiShoppingCartLight } from "react-icons/pi";
 import { LiaBarsSolid } from "react-icons/lia";
@@ -7,40 +7,81 @@ import logo from '../../Assets/Logo.png';
 import "./NavBar.scss";
 
 const Navbar = () => {
+  // Definindo estados para controle da sidebar e tamanho da tela
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const navigate = useNavigate(); 
+  const [screenSize, setScreenSize] = useState({
+    isMobile: window.innerWidth <= 600,
+    isFablet: window.innerWidth > 600 && window.innerWidth <= 750,
+    isTablet: window.innerWidth > 750 && window.innerWidth <= 990,
+    isDesktop: window.innerWidth > 990,
+  });
+  const navigate = useNavigate();
 
+  // Função para alternar o estado da sidebar
   const toggleSidebar = () => {
-    setIsSidebarOpen(!isSidebarOpen);
+    // Usando a versão funcional de setState para garantir imutabilidade
+    setIsSidebarOpen((prevState) => !prevState);
   };
 
   const goToHomePage = () => {
-    navigate("/"); 
+    navigate("/");
   };
+
+  // Atualiza o estado do tamanho da tela de forma funcional e imutável
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenSize((prevSize) => ({
+        ...prevSize,
+        isMobile: window.innerWidth <= 600,
+        isFablet: window.innerWidth > 600 && window.innerWidth <= 750,
+        isTablet: window.innerWidth > 750 && window.innerWidth <= 990,
+        isDesktop: window.innerWidth > 990,
+      }));
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-left" onClick={goToHomePage}>
           <img src={logo} alt="Logo" className="navbar-logo" />
-          <div className="logo">DroopShop</div>
+          {(screenSize.isDesktop || screenSize.isTablet || screenSize.isFablet ) && (
+            <>
+                <div className="logo">DroopShop</div>
+            </>
+          )}
+          
         </div>
 
-        <div className="navbar-center">
-          <div className="search-bar">
-            <BiSearch className="search-icon" />
-            <input
-              type="text"
-              placeholder="Anúncio, usuário ou categoria"
-              className="search-input"
-            />
+        {/* Exibe a barra de busca em tablets e desktops */}
+        {(screenSize.isTablet || screenSize.isDesktop) && (
+          <div className="navbar-center">
+            <div className="search-bar">
+              <BiSearch className="search-icon" />
+              <input
+                type="text"
+                placeholder="Anúncio, usuário ou categoria"
+                className="search-input"
+              />
+            </div>
           </div>
-        </div>
+        )}
 
         <div className="navbar-right">
           <ul className="navbar-menu">
-            <li className="navbar-item">Categorias</li>
-            <li className="navbar-item">Blog</li>
+            {/* Exibe apenas em telas desktop */}
+            {(screenSize.isTablet || screenSize.isDesktop) && (
+              <>
+                <li className="navbar-item itens-text">Categorias</li>
+                <li className="navbar-item itens-text">Blog</li>
+              </>
+            )}
             <li className="navbar-item">
               <button className="anunciar-button">Anunciar</button>
             </li>
@@ -63,8 +104,15 @@ const Navbar = () => {
           &times;
         </button>
         <ul>
+
           <li>Entrar</li>
           <li>Registrar</li>
+          {(screenSize.isMobile || screenSize.isTablet || screenSize.isFablet) && (
+            <>
+              <li>Categoria</li>
+              <li>Blog</li>
+            </>
+          )}
           <li>Sobre</li>
         </ul>
       </div>
